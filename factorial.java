d6a69d30b671c5bbe9bfe888c272579236e06a3c
kanaklatwal <kanaklatwal@gmail.com>
2026-01-21 09:37:22 +0530

    Factorial of a number

---
+++ factorial.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class factorial {
+    public static int factorialNum(int n){
+        int fact = 1;
+        for(int i=1;i<=n;i++){
+            fact *= i;
+        }
+        return fact;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a nnumber");
+        int num = sc.nextInt();
+        int factorial = factorialNum(num);
+        System.out.println("The factorial of "+num+ " is " + factorial);
+    }
+}
