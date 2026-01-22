2a7166e1702b116842ba3999356abd3935086c72
kanaklatwal <kanaklatwal@gmail.com>
2026-01-22 22:31:40 +0530

    Count the Frequency of characters in the string

---
+++ countFrequency.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class countFrequency {
+
+    public static void count(String s) {
+        boolean[] visited = new boolean[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            int count = 1;
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println("The frequency of " + s.charAt(i) + " is " + count);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String: ");
+        String str = sc.nextLine();
+        count(str);
+    }
+}
