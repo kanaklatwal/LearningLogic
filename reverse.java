8bc90ddaf5e2c860ddca75a2a12fa8adc697ea1c
kanaklatwal <kanaklatwal@gmail.com>
2026-01-22 13:34:04 +0530

    To reverse a number

---
+++ reverse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class reverse{
+    public static int reverseNumber(int num){
+        int rem;
+        int digit = 0;
+        while(num!=0){
+            rem = num%10;
+            digit = digit*10 + rem;
+            num = num/10;
+         }
+         return digit;
+    }
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number ");
+        int n = sc.nextInt();
+        System.out.println("The reversed number of "+n+" is "+ reverseNumber(n));
+    }
+}
