cf892af1f627321755fe1279692373f2332de2bd
kanaklatwal <kanaklatwal@gmail.com>
2026-01-25 17:34:07 +0530

    Remove Duplicate elements from the sorted arrays

---
+++ Arrays/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+// Remove duplicates from arrays
+package Arrays;
+public class RemoveDuplicates{
+    public static int removeDuplicates(int[] arr){
+        int i =0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[j]!=arr[i]){
+                i++;
+                arr[i] =arr[j];
+            }
+        }
+        return i+1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 3, 4, 4};
+        int k = removeDuplicates(arr);
+        System.out.println("The array has " + k +" unique elements");
+        System.out.println("The Array is : ");
+        for(int i =0; i<k; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
