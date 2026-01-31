9b20142d3df676a6fb3c172bf3e237ef2403f5b4
kanaklatwal <kanaklatwal@gmail.com>
2026-01-31 15:55:52 +0530

    Implementation of Linked  List in java

--- LinkedList/implementation.java
+++ LinkedList/implementation.java
@@ -30,6 +30,38 @@ public class implementation{
                 head =  temp;
             }
          }
+         void insertAtIndex(int idx, int val){
+            Node t = new Node(val);
+            Node temp = head;
+            if(idx==size()){
+                insertAtEnd(val);
+                return;
+            }
+            else if(idx==0){
+                insertAtBeginning(val);
+                return;
+            }
+            else if(idx<0||idx>size()){
+                 System.out.println("Wrong index");
+                 return;
+            }          
+            for(int i = 1; i<=idx-1; i++ ){
+                temp = temp.next;
+            }
+            t.next = temp.next;
+            temp.next = t;  
+         }
+         int getAt(int idx){
+            Node temp = head;
+            if(idx<0||idx>size()){
+                System.out.println("Wrong index");
+                return -1;
+            }  
+            for (int i = 0; i <= idx; i++) {
+                temp = temp.next;
+            }
+            return temp.data;
+         }
          void display(){
             Node temp = head;
             while(temp!=null){
@@ -58,7 +90,15 @@ public class implementation{
         ll.insertAtEnd(14);
         ll.display();// 4 -> 5 -> 14
         ll.insertAtBeginning(15);
-        ll.display();
-
+        ll.display();// 15-> 4 -> 5-> 14
+        ll.insertAtIndex(2, 10);
+        ll.display();// 15 -> 4->10->5->14
+        ll.insertAtIndex(4, 17 );
+        ll.display();//
+        System.out.println(ll.tail.data);//14
+        ll.insertAtIndex(0, 100);
+        ll.display();// 100->15 -> 4->10->5->14
+        System.out.println(ll.getAt(4));//17
+        
     }
 }
