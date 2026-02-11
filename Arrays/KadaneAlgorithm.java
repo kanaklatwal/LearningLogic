10a410b37dac948b42b79fa18420102ac57ca3d2
kanaklatwal <kanaklatwal@gmail.com>
2026-02-11 16:56:52 +0530

    Kadane's Algorithm

---
+++ Arrays/KadaneAlgorithm.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class KadaneAlgorithm {
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3 , -5 , 5, 7, 8};
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+        for(int i=1; i<arr.length; i++){
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+    }
+}
