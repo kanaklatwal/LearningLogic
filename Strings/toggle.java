ddb2946dad37376984e2ebcc5db544bdd4113dfc
kanaklatwal <kanaklatwal@gmail.com>
2026-02-13 18:27:37 +0530

    Toggle the string with the help of string builder

---
+++ Strings/toggle.java
@@ -0,0 +1,30 @@
+package Strings;
+import java.util.Scanner;
+public class toggle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        StringBuilder str = new StringBuilder(sc.nextLine());
+        System.out.println(str);
+        //toggle     PHysiCs -> phYSIcS
+        for(int i=0; i<str.length(); i++){
+            // P->p
+            // check -> alphabet - small, capital
+            boolean flag = true;// true-> capital
+            char ch = str.charAt(i); //A
+            if(ch == ' ') continue;
+            int asci = (int)ch; //65
+            if(asci>=97) flag = false;// small
+            if(flag == true){// capital
+                asci += 32;
+                char dh = (char)asci; //a
+                str.setCharAt(i, dh);
+            }
+            else{  //small
+                asci -= 32;
+                char dh = (char)asci;
+                str.setCharAt(i, dh);
+            }
+        }
+        System.out.println(str);
+    }
+}
