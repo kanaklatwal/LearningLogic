e9b793cdb84e3969fb60c2f3c73632e963663813
kanaklatwal <kanaklatwal@gmail.com>
2026-02-13 20:45:20 +0530

    To find the palindromic substring in String

---
+++ Strings/palindromicSubstring.java
@@ -0,0 +1,29 @@
+// Given a string s, return the number of palindromic substrings in it.   input s="abc"  output: 3
+package Strings;
+
+public class palindromicSubsrting {
+    public static boolean isPalindrome(String s){
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "abcd";
+        int count =0;
+        for(int i=0; i<str.length(); i++){
+            for(int j=i+1;j<=str.length(); j++){
+                if(isPalindrome(str.substring(i,j)) == true){
+                    System.out.print(str.substring(i,j) + " ");
+                    count++;
+                }
+            }
+            System.out.println("\n The number of palindromic substring are: " + count);
+        }
+    }
+    
+}
