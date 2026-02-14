eb247066ea5a2e77991a85c92cc69998b44bcc53
kanaklatwal <kanaklatwal@gmail.com>
2026-02-14 17:40:39 +0530

    Reverse the words in sentence

---
+++ Strings/reverseWord.java
@@ -0,0 +1,23 @@
+// Java Program to reverse each word in string
+// input: I am an Student    output:- I ma na tenduts
+public class reverseWord {
+    public static void main(String[] args){
+        String str = "I am an student";
+        String ans = "";
+        StringBuilder sb  = new StringBuilder("");
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch !=' '){
+                sb.append(ch);
+            }else{
+                sb.reverse();
+                ans += sb;
+                ans += " ";
+                sb = new StringBuilder("");
+            }
+        }
+        sb.reverse();
+        ans += sb;
+        System.out.print(ans);
+    }
+}
