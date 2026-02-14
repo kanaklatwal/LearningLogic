9a8752b361c54abff05aedf5a1f5d7879b62efad
kanaklatwal <kanaklatwal@gmail.com>
2026-02-14 18:45:27 +0530

    Duplicate characters replaced

---
+++ Strings/duplicateCharacters.java
@@ -0,0 +1,27 @@
+// the string should be compressed such that consecutive duplicates
+//of characters are replaced with the character and followed by the number
+// of consecutive duplicates. input:- aaabbbbccddde   output:  a3b4c2d3e
+package Strings;
+
+public class duplicateCharacters {
+    public static void main(String[] args){
+        String str = "aaabbbbccddde";
+        String ans = "" + str.charAt(0);
+        int count =1;
+        for(int i=1; i<str.length(); i++){
+            char curr =  str.charAt(i);
+            char prev = str.charAt(i-1);
+            if(curr == prev){
+                count++;
+            }
+            else{
+                if(count > 1) ans+=count;
+                ans+=count;
+                count = 1;
+                ans += curr;  
+            }
+        }
+        if(count > 1) ans += count;
+        System.out.println(ans);
+    }
+}
