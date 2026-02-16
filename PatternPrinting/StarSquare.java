59465832b83d89cdaa0d50c39bde9f00024f981c
kanaklatwal <kanaklatwal@gmail.com>
2026-02-16 14:09:17 +0530

    Print Square of *

---
+++ PatternPrinting/StarSquare.java
@@ -0,0 +1,16 @@
+package PatternPrinting;
+import java.util.Scanner;
+public class StarSquare {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the star pattern: ");
+        int size = sc.nextInt();
+        for(int i=0; i<size; i++){
+            for(int j=0;j<size; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
