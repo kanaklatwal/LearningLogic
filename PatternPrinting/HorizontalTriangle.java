39f22f945d89d77f686567e01fa90f0a1c06312e
kanaklatwal <kanaklatwal@gmail.com>
2026-02-17 16:20:59 +0530

    Star Triangle Horizontally Flipped

--- PatternPrinting/HorizontalTriangle.java
+++ PatternPrinting/HorizontalTriangle.java
@@ -5,8 +5,14 @@ public class HorizontalTriangle {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size : ");
         int size = sc.nextInt();
-        for(int i=size; i>=1; i--){
-            for(int j=i; j>=1; j--){
+//        for(int i=size; i>=1; i--){
+//            for(int j=i; j>=1; j--){
+//                System.out.print("*" + " ");
+//            }
+//            System.out.println();
+//        }
+        for(int i=1; i<=size; i++){
+            for(int j=1;j<=size+1-i; j++){
                 System.out.print("*" + " ");
             }
             System.out.println();
