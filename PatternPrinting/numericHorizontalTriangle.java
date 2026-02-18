603f685b1b9f6549a0f6ff29221a4acbd3bf5da9
kanaklatwal <kanaklatwal@gmail.com>
2026-02-18 22:55:37 +0530

    Number Triangle Horizontal Flipped

---
+++ PatternPrinting/numericHorizontalTriangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class numericHorizontalTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size:  ");
+        int size = sc.nextInt();
+        for (int i=1;i<=size; i++){
+            for(int j=1; j<=size+1-i; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
