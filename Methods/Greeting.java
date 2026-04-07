a7915f842deece3ba086d7e969b695c8c566b142
kanaklatwal <kanaklatwal@gmail.com>
2026-04-07 07:30:42 +0530

    Methods/ functions

---
+++ Methods/Greeting.java
@@ -0,0 +1,12 @@
+package Methods;
+
+import java.sql.SQLOutput;
+
+public class Greeting {
+    public static void main(String[] args) {
+        greeting();
+    }
+    static void greeting(){
+        System.out.println("Hello World");
+    }
+}
