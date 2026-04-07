6fb7d88837f1e6ea1b5894b6290e84f85782eca8
kanaklatwal <kanaklatwal@gmail.com>
2026-04-07 07:34:42 +0530

    Methods / Functions

--- Methods/Sum.java
+++ Methods/Sum.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Sum{
     public static void main(String[] args){
         sum();
+        int ans = sum2();
     }
     static void sum(){
         Scanner in = new Scanner(System.in);
@@ -12,6 +13,14 @@ public class Sum{
         int num2 = in.nextInt();
         int sum = num1 + num2;
         System.out.println(" the sum = "+ sum);
-
+    }
+    static int sum2() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter  number 1: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter  number 2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return sum;
     }
 }
