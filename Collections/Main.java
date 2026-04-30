4b6f0c8356a48a0b08474a8a6dc626ca7adab17e
kanaklatwal <kanaklatwal@gmail.com>
2026-04-30 21:16:01 +0530

    feat: add sorting algorithms for enhanced data manipulation

--- Collections/Main.java
+++ Collections/Main.java
@@ -2,7 +2,9 @@ package Collections;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         // List or Collection -> Interface
@@ -42,6 +44,85 @@ public class Main {
           while(iterator.hasNext()){
                 System.out.println("Element : "+ iterator.next());
           }
+
+          List<Integer> list3 = new ArrayList<>();
+          list3.add(11);
+          list3.add(12);
+          list3.add(14);
+          System.out.println(list3.get(1));//12
+          System.out.println(list3.get(2));//14
+
+          System.out.println("before set: " + list3);//[11, 12, 14]
+          list3.set(0,100);     //set(index, value);
+          System.out.println("After set: " + list3);//[100, 12, 14]
+
+          //toArray
+          Object[] arr = list3.toArray();
+          for(Object obj: arr){
+                System.out.println(obj); // 100 12 14
+          }
+
+          //contains
+          System.out.println(list3.contains(100)); //true
+          System.out.println(list3.contains(1000)); //false
+
+          //Collection<Integer> collection = new ArrayList<>();
+
+          list1.add(12);
+          list1.add(6);
+          System.out.println("Printing Entire List " + list1);
+
+          //sort
+          Collections.sort(list1);
+          System.out.println("Printing Entire Sorted list "+ list1);
+
+          ArrayList<Integer> newList = (ArrayList<Integer>)list1.clone();
+          System.out.println("Printing the new List : " + newList);
+
+          ArrayList<Integer> marks = new ArrayList<>();
+          marks.ensureCapacity(100);
+
+          System.out.println(newList.isEmpty());//false
+          System.out.println(marks.isEmpty()); //true
+
+          System.out.println(newList.indexOf(40));  //4
+
+          // linkedlist
+
+          LinkedList<Integer> list = new LinkedList<>();
+          list.add(1);
+          list.add(2);
+          list.add(3);
+          list.add(4);
+          System.out.println(list);
+          list.remove(3);
+          System.out.println(list);
+          System.out.println(list.indexOf(4));
+
+          LinkedList<Integer> llist = new LinkedList<>();
+          llist.add(5);
+          llist.add(6);
+          llist.add(7);
+          System.out.println( list.addAll(llist));
+          System.out.println("New linked list is: " +list);
+          System.out.println(list.removeAll(llist));
+
+          //addFirst() and addLast()
+          System.out.println("Printing original list : "+list);
+          list.remove(2);
+          System.out.println("Printing original list : "+list);
+          list.addFirst(101);
+          System.out.println(list);
+          list.addLast(98);
+          System.out.println(list);
+          list.removeFirst();
+          list.removeLast();
+          System.out.println(list);
+          System.out.println(list.peek());
+          System.out.println(list.poll());
+          System.out.println(list);
+          System.out.println(list.offer(89));
+
     }
 
 }
