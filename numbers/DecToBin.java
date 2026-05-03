bfdb86d3fd5e55b0b9312e95023a0c891cceaf50
kanaklatwal <kanaklatwal@gmail.com>
2026-05-03 19:08:52 +0530

    feat: implement main method for decimal to binary conversion in DecToBin class

--- numbers/DecToBin.java
+++ numbers/DecToBin.java
@@ -1,4 +1,24 @@
 package numbers;
+import java.util.Scanner;
+import static java.lang.Math.pow;
+
+import static java.lang.Math.pow;
 
 public class DecToBin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter n: ");
+        n = sc.nextInt();
+        int ans = 0;
+        int i=0;
+        while( n!= 0){
+            int bit = n&1;
+            ans = (int) ((bit*pow(10,i)) + ans);
+            n = n>>1;
+            i++;
+        }
+        System.out.println("Answer is: " + ans);
+    }
+
 }
