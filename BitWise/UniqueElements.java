48e41da098a4f751379a15d8e770d2d626d29379
kanaklatwal <kanaklatwal@gmail.com>
2026-05-09 16:07:53 +0530

    feat: add UniqueElements class to find unique elements using bitwise XOR

---
+++ BitWise/UniqueElements.java
@@ -0,0 +1,16 @@
+package BitWise;
+
+public class UniqueElements {
+    static int findUnique(int[] arr){
+        int ans = 0;
+        for(int num : arr){
+            ans ^= num;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr ={2, 2, 5, 8, 9, 5, 7, 8, 9};
+        System.out.println("Unique ELments: " + findUnique(arr));
+    }
+}
