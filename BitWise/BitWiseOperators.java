a1648ba77ec1b4cc42ada35d5f816e925ba8741f
kanaklatwal <kanaklatwal@gmail.com>
2026-05-09 11:30:29 +0530

    feat: update BitWiseOperators and EvenAndOdd classes with new bitwise operations demonstration

--- BitWise/BitWiseOperators.java
+++ BitWise/BitWiseOperators.java
@@ -8,11 +8,23 @@ public class BitWiseOperators {
 //        System.out.println( a | b);//7
 //        System.out.println( a ^ b);//3
 //        System.out.println(~a);//-6
-          int n = 5;
-          for(int i = 1; i<=32; i++){
-              n = n<<1;
-              System.out.println(n);
-              System.out.println();
-          }
+//          int n = 5;
+//          for(int i = 1; i<=32; i++){
+//              n = n<<1;
+//              System.out.println(n);
+//              System.out.println();
+//          }
+//        int n =100;
+//        for(int i =1; i<=10; i++){
+//            n = n>>1; //right Shift
+//            System.out.println(n);
+//            System.out.println();
+//        }
+        int n =100;
+        for(int i =1; i<=10; i++){
+            n = n>>>1; // Unsigned Right shift
+            System.out.println(n);
+            System.out.println();
+        }
     }
 }
--- BitWise/EvenAndOdd.java
+++ BitWise/EvenAndOdd.java
@@ -1,4 +1,13 @@
 package BitWise;
 
 public class EvenAndOdd {
+    public static void main(String[] args) {
+        int n= 11;
+        if((n&1) ==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
 }
