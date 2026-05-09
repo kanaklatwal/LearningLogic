c5d08383c11a6e689fcddec4f3cf44a8a57d15a4
kanaklatwal <kanaklatwal@gmail.com>
2026-05-09 13:48:08 +0530

    feat: simplify power of 2 check in PowerOf2 class

--- BitWise/PowerOf2.java
+++ BitWise/PowerOf2.java
@@ -1,4 +1,27 @@
 package BitWise;
 
 public class PowerOf2 {
+    public static void main(String[] args) {
+        int n = 5;
+//        int count = 0;
+//        while(n!=0){
+//            if((n&1) == 1){
+//                count++;
+//            }
+//            n = n>>1;
+//        }
+//        System.out.println("Set bit Count: " + count);
+//        if(count == 1){
+//            System.out.println("Number "+ n+ " is power of 2" );
+//        }
+//        else{
+//            System.out.println("Not a power of 2");
+//        }
+        if((n & (n-1)) == 0){
+            System.out.println("the number is power of 2");
+        }
+        else{
+            System.out.println("the number is not power of 2");
+        }
+    }
 }
