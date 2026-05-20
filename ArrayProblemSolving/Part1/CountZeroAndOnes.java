af9cdfaca1baeadabccb265a99aa5d7ea4008616
kanaklatwal <kanaklatwal@gmail.com>
2026-05-20 23:34:32 +0530

    feat: add CountZeroAndOnes class to count zeros and ones in an array

---
+++ ArrayProblemSolving/Part1/CountZeroAndOnes.java
@@ -0,0 +1,26 @@
+package ArrayProblemSolving.Part1;
+
+public class CountZeroAndOnes {
+    static int[] getZeroCount(int[] arr){
+        int zeroCount = 0;
+        int oneCount = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==0){
+                zeroCount++;
+            }
+            else{
+                oneCount++;
+            }
+        }
+        int ans[] = {zeroCount, oneCount};
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 0, 1, 1, 0, 1, 1};
+        int ans[] = getZeroCount(arr);
+        System.out.println("Zero Count = "+ ans[0]);
+        System.out.println("One Count = "+ ans[1]);
+    }
+
+}
