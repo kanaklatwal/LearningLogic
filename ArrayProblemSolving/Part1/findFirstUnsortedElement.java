7bcea6d6c84463f06d0833b76e8994f307d5a779
kanaklatwal <kanaklatwal@gmail.com>
2026-05-20 23:52:25 +0530

    feat: add findFirstUnsortedElement class to identify the first unsorted element in an array

---
+++ ArrayProblemSolving/Part1/findFirstUnsortedElement.java
@@ -0,0 +1,18 @@
+package ArrayProblemSolving.Part1;
+
+public class findFirstUnsortedElement {
+    static int getUnsortedElement(int[] arr){
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i+1]<=arr[i]){
+                return arr[i+1];
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 5, 4, 9};
+        System.out.println(getUnsortedElement(arr));
+    }
+}
