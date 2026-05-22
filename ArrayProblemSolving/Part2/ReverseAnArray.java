62a254d6442ac3f4120bac0c1c4d21f4131eeb79
kanaklatwal <kanaklatwal@gmail.com>
2026-05-22 13:25:04 +0530

    feat: add ReverseAnArray class to reverse the elements of an array

---
+++ ArrayProblemSolving/Part2/ReverseAnArray.java
@@ -0,0 +1,25 @@
+package ArrayProblemSolving.Part2;
+
+public class ReverseAnArray {
+    static  void getReverse(int[] arr){
+        int n = arr.length;
+        int i = 0;
+        int j=n-1;
+        while(i<=j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] =  temp;
+            i++;
+            j--;
+        }
+        for(int k : arr){
+            System.out.print(k + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4, 2, 5, 9, 7};
+        getReverse(array);
+    }
+}
