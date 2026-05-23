c1fcaff42850c30e3d6dfd005805a12ebcebd178
kanaklatwal <kanaklatwal@gmail.com>
2026-05-23 20:29:00 +0530

    feat: implement getUniqueElements method to find unique elements using XOR

--- ArrayProblemSolving/Part2/findUniqueElements.java
+++ ArrayProblemSolving/Part2/findUniqueElements.java
@@ -1,4 +1,16 @@
 package ArrayProblemSolving.Part2;
 
 public class findUniqueElements {
+    public static int getUniqueElements(int[] nums){
+        int xorSum = 0;
+        for(int n: nums){
+            xorSum = xorSum ^ n;
+        }
+        return xorSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr= {1,1,2,2,3,4,4,5,5,6,6};
+        System.out.println(getUniqueElements(arr));
+    }
 }
