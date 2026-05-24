faad8efd080c9482ba96ff7939eaccd0ef79f501
kanaklatwal <kanaklatwal@gmail.com>
2026-05-24 14:52:52 +0530

    feat: add TwoSum class to find indices of two numbers that add up to a target

---
+++ ArrayProblemSolving/Part3/TwoSum.java
@@ -0,0 +1,23 @@
+package ArrayProblemSolving.Part3;
+
+public class TwoSum {
+    public static int[] getTwoSum(int[] nums, int target){
+        int n = nums.length;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i] + nums[j] == target){
+                    int ans[] = {i, j};
+                    return ans;
+                }
+            }
+        }
+        int ans[] = {};
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr= {2,7,4,3,6};
+        int target = 10;
+        System.out.println(getTwoSum(arr, target));
+    }
+}
