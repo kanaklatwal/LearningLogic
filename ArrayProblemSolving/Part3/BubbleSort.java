95f1097ea33f109ce8b1d695194f25a30b378d3c
kanaklatwal <kanaklatwal@gmail.com>
2026-05-28 16:32:25 +0530

    feat: add BubbleSort class to implement bubble sort algorithm for array sorting

---
+++ ArrayProblemSolving/Part3/BubbleSort.java
@@ -0,0 +1,24 @@
+package ArrayProblemSolving.Part3;
+
+public class BubbleSort {
+    public static void getBubbleSort(int[] arr){  // Time complexity :-  O(n^2)
+        for(int i=0; i<arr.length-1; i++){ // Rounds
+            for(int j=0; j<arr.length-i-1; j++){  // neighbouring element comparison
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6,5,1,3};
+        getBubbleSort(arr);
+        System.out.println("Printing the array: ");
+        for(int value: arr){
+            System.out.print(value + " ");
+        }
+    }
+}
